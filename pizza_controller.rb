c452e081e0c062678251525d363ac8eb30ae9982
yingying <chen.yingying.01988@gmail.com>
2018-08-07 13:23:15 +0100

    RESTful_Pizza_Shop

---
+++ pizza_controller.rb
@@ -0,0 +1,30 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative ( './models/pizza_order')
+
+also_reload ( './models/*')
+# Sinatra function to reload file everytime
+
+
+# INDEX route___create a route for people can see pizza orders
+get '/pizza-orders' do
+  # beaware its pizza-order not pizza_order
+  @orders = PizzaOrder.all()
+  erb (:index)
+end
+
+# NEW route (Create)
+get '/pizza-orders/new' do
+  erb(:new)
+end
+
+# SHOW route
+get '/pizza-orders/:id' do
+  @order = PizzaOrder.find(params[:id])
+  erb(:show)
+
+end
+
+#
